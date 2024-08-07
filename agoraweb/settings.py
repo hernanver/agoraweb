1c97af606e35f3385473f53594def16eadbb2c77
hernanver <hernanverzini@hotmail.com>
2024-08-07 08:22:00 -0300

    Fixing statics

--- agoraweb/settings.py
+++ agoraweb/settings.py
@@ -75,7 +75,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "core.context_processors.site_url",
             ],
         },
     },
@@ -94,10 +93,7 @@ WSGI_APPLICATION = "agoraweb.wsgi.application"
 #     }
 # }
 
-import dj_database_url 
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-}  
+
 
 
 # Password validation
@@ -128,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -136,11 +131,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 import django_heroku
 
@@ -148,7 +144,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
-
+import dj_database_url 
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}  
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -157,5 +156,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tu-email@gmail.com'
 EMAIL_HOST_PASSWORD = 'tu-password'
 
-
-SITE_URL = 'http://127.0.0.1:8000'
