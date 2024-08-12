9b88b22243612836ccb5165baad1e9d8c4076ef4
hernanver <hernanverzini@hotmail.com>
2024-08-12 04:36:12 -0300

    Last Updates

--- agoraweb/settings.py
+++ agoraweb/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['agoratech-ai.com', 'www.agoratech-ai.com', 'agoraweb-7d7dc24c3bf7.herokuapp.com']
 
 
 # Application definition
@@ -141,6 +141,7 @@ import django_heroku
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 django_heroku.settings(locals())
 
 import dj_database_url 
@@ -156,9 +157,9 @@ EMAIL_HOST_USER = 'tu-email@gmail.com'
 EMAIL_HOST_PASSWORD = 'tu-password'
 
 
-#SECURE_SSL_REDIRECT = True
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#SECURE_HSTS_SECONDS = 31536000  # 1 año
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#SECURE_HSTS_PRELOAD = True
-#SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 31536000  # 1 año
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
