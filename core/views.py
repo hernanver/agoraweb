1f5783c4b8edd8382f0e189400ea2364fb0f1343
hernanver <hernanverzini@hotmail.com>
2024-09-30 04:40:17 -0300

    Video

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import ContactForm
+from django.utils.translation import get_language
+from django.templatetags.static import static
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
@@ -13,6 +15,26 @@ class HomePageView(TemplateView):
         context['form'] = ContactForm()
         context['success'] = False
         context['request'] = self.request
+
+
+        # Obtener el idioma actual
+        language = get_language()
+
+        # Determinar la ruta del video según el idioma
+        if language == 'ca':
+            video_url = static('core/videos/video-catalan.mp4')
+        elif language == 'es':
+            video_url = static('core/videos/video-castellano.mp4')
+        elif language == 'en':
+            video_url = static('core/videos/video-ingles.mp4')
+        else:
+            video_url = static('core/videos/video-castellano.mp4')  # Video por defecto
+
+        # Pasar la URL del video al contexto
+        context['video_url'] = video_url
+
+        return context
+
         return context
 
     def post(self, request, *args, **kwargs):
